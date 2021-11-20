f59a4f878b294385690e078fe92f325e0c02a014
Da18ne4ka <DEeev2003@yandex.ru>
2021-11-20 11:06:04 +0300

    end

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
-import sys
-
-from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtGui import QPainter, QColor
+import sys
 from random import randint
 
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(300, 300)
+        self.btn = QPushButton(self)
+        self.btn.resize(100, 70)
+        self.btn.move(150, 100)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-class MyWidget(QMainWindow):
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.btn.setText(_translate("MainWindow", "Нажми меня"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
+        # остальное без изменений
+        self.setupUi(self)
         self.do_paint = False
-        self.btn.clicked.connect(self.draw_circle)
+        self.btn.clicked.connect(self.paint)
 
     def paintEvent(self, event):
-        # Создаем объект QPainter для рисования
-        qp = QPainter()
         if self.do_paint:
-            # Начинаем процесс рисования
+            qp = QPainter()
             qp.begin(self)
-            self.draw(qp)
-            # Завершаем рисование
+            self.draw_flag(qp)
             qp.end()
 
     def paint(self):
         self.do_paint = True
         self.repaint()
 
-    def draw_circle(self, qp):
-        # Задаем кисть
-        qp.setBrush(QColor(255, 255, 0))
-        qp.setPen(QColor(255, 255, 0))
-        # Рисуем прямоугольник заданной кистью
+    def draw_flag(self, qp):
+        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+        qp.setBrush(QColor(r, g, b))
+        qp.setPen(QColor(r, g, b))
         size = randint(0, 200)
-        qp.drawEllipse(50, 70, size, size)
+        qp.drawEllipse(30, 60, size, size)
 
 
 if __name__ == '__main__':
